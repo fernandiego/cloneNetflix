d4c363b64245aea5b188dca64d8c4349a834857b
fernandiego <fernandodiego.sn@gmail.com>
2021-06-16 23:28:51 -0300

    na peia pra exibir o json do endpoint /films

--- server/routes/listFilms.js
+++ server/routes/listFilms.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const listF = express()
 const axios = require('axios')
+const request = require('request')
 
+const baseURL = "https://ghibliapi.herokuapp.com/films"
 
-listF.get('/', async (req, res) => {
-	res.send('get listfilms')
+request(`${baseURL}`, async (req, res, body) => {
+let ret = body.json()	
+console.log(ret)
+res.send(ret)
 })
 
 exports.listF = listF;
