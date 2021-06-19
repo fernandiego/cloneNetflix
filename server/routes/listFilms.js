2ef6e1d81a65adba9d69fa3a0f07c66dbbd1e65f
fernandiego <fernandodiego.sn@gmail.com>
2021-06-19 19:09:27 -0300

    its alive hahaha endpoint funcionando

--- server/routes/listFilms.js
+++ server/routes/listFilms.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const listF = express()
+const axios = require('axios')
+const baseURL = "https://ghibliapi.herokuapp.com"
+const api = axios.create({baseURL})
 
 
 listF.get('/', async (req, res) => {
-	res.send('get films')
+	const ret = await axios.get("https://ghibliapi.herokuapp.com/films")
+	console.log(ret.data)
+	res.send(ret.data)
 })
+
+// https://ghibliapi.herokuapp.com/films 
 
 exports.listF = listF;
